7e90f14c14add882f36ef2897c445b38ac9f2cdc
Brandon SORET <viliprant@gmail.com>
2025-12-25 15:24:00 +0100

    Ajout de la gestion de la sécurité avec Helmet et limitation du taux de requêtes avec express-rate-limit

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,18 @@ import morgan from 'morgan';
 import { errorHandler } from './middlewares/errorHandler';
 import authRoutes from './routes/auth.routes';
 import { authHandler } from './middlewares/authHandler';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // 100 requêtes / IP
+});
+
+app.use(limiter);
+app.use(helmet());
 app.use(morgan('dev'));
 
 app.use(express.json());
